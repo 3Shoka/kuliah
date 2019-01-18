08b8c674783e9d4df51b17b8caff17b8bc1ff2bb
Ashokani A <hi@ashz.xyz>
2019-01-18 20:17:55 +0700

    hello world java

---
+++ src/DataMhs.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author ashoka
+ * Created time 7:15:56 PM 
+ * Date Sep 14, 2018
+ */
+public class DataMhs {
+    public static void main(String[] args) {
+        System.out.println("NIM: 18.01.55.2032");
+        System.out.println("NAMA: Ashokani");
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author ashoka
+ * Created time 6:43:56 PM 
+ * Date Sep 14, 2018
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
