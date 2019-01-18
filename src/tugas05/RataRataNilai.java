3204361b4f1017d7d2c5ee0edd9169360752eca8
Ashokani A <hi@ashz.xyz>
2019-01-18 20:50:25 +0700

    tugas 05 rata rata nilai

---
+++ src/tugas05/RataRataNilai.java
@@ -0,0 +1,37 @@
+package tugas05;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ashokani
+ * Created time 8:16:52 AM
+ * Date Dec 19, 2018
+ * 
+ * Buat program untuk menghitung nilai rata-rata mahasiswa 
+ * menggunakan IO. 
+ * dari 3 nilai yang diinputkan user.
+ * Nilai yang diinput menggunakan looping
+ */
+public class RataRataNilai {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama, nim;
+        double nilai[] = new double[3], total = 0, rata;
+        
+        System.out.print("Nama Mahasiswa      : ");
+        nama = sc.nextLine();
+        System.out.print("NIM Mahasiswa       : ");
+        nim = sc.nextLine();
+        for(int i=0;i<nilai.length;i++){
+            System.out.print("Masukkan nilai ke-"+ (i+1) +" : ");
+            nilai[i] = sc.nextDouble();
+            total += nilai[i];
+        }
+        
+        rata = total/nilai.length;
+        System.out.println("Total nilai         : "+total);
+        System.out.println("Rata-rata nilai     : "+ String.format("%.2f", rata));
+
+    }
+}
