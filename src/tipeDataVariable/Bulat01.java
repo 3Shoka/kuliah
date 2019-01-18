08fc0e601f1266d6e0482cec713d48859962f9ea
Ashokani A <hi@ashz.xyz>
2019-01-18 20:23:38 +0700

    tipe data dan variabel

---
+++ src/tipeDataVariable/Bulat01.java
@@ -0,0 +1,38 @@
+package tipeDataVariable;
+
+/**
+ *
+ * @author ashoka
+ * Created time 7:07:47 PM 
+ * Date Sep 19, 2018
+ */
+public class Bulat01 {
+    public static void main(String[] args) {
+        // deklarasi variable
+        byte b;
+        int i;
+        short s;
+        long l;
+        double d;
+        char c;
+        String S;
+        
+        // inisialisasi nilai
+        b = 30;
+        i = 12;
+        s = 23;
+        l = 11;
+        d = 2.3;
+        c = 68;
+        S = "Ashokani";
+        
+        // menampilkan nilai
+        System.out.println("nilai dari variable byte `b` = "+b);
+        System.out.println("nilai dari variable int `i` = "+i);
+        System.out.println("nilai dari variable short `s` = "+s);
+        System.out.println("nilai dari variable long `l` = "+l);
+        System.out.println("nilai dari variable double `d` = "+d);
+        System.out.println("nilai dari variable char `c` = "+c);
+        System.out.println("nilai dari variable String `S` = "+S);
+    }
+}
