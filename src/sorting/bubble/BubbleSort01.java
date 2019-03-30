b8b2110bffb6f155a793e35b529bd3520c3ed8ab
Ashokani <hi@ashz.xyz>
2019-03-30 10:55:23 +0700

    menambahkan tampilan proses sorting sementara

--- src/sorting/bubble/BubbleSort01.java
+++ src/sorting/bubble/BubbleSort01.java
@@ -6,35 +6,33 @@ package sorting.bubble;
  */
 public class BubbleSort01 {
     public static void main(String[] args) {
-        int data[] = {8,2,7,5};
-
+        int data[] = {5,6,8,1,3,25,10};
         // cetak data sebelum di sorting
         System.out.println("data sebelum di sorting");
         for (int i = 0; i < data.length; i++) {
             System.out.print(data[i]+" ");
         }
-
+        System.out.println();
         // sorting bubble
-        for (int i = 1; i < data.length; i++) {
-            for (int j = data.length-1; j >= 1; j--) {
-                if (data[j] < data[j-1]) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("proses "+(i+1));
+            for (int j = 0; j < data.length-1; j++) {
+                if (data[j] > data[j+1]) {
                     // jika benar, ditukar
                     int tmp;
                     tmp = data[j];
-                    data[j] = data[j-1];
-                    data[j-1] = tmp;
+                    data[j] = data[j+1];
+                    data[j+1] = tmp;
                 }
+                // cetak data sorting sementara
+                // System.out.println("sementara "+data.length);
+                for (int t = 0; t < data.length; t++) {
+                    System.out.print(data[t]+" ");
+                }
+                System.out.println();
             }
-            System.out.println();
-            // cetak data sorting sementara
-            System.out.println("data sorting sementara");
-            for (int t = 0; t < data.length; t++) {
-                System.out.print(data[t]+" ");
-            }
-
         }
-        System.out.println();
-
+        
         // cetak data setelah di sorting
         System.out.println("data setelah sorting");
         for (int i = 0; i < data.length; i++) {
